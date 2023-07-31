be56421c9f448e3d79b0a63bc67d020ecef90eef
Efe Mert Yalcin <efemertyalcin@gmail.com>
2023-07-31 17:17:10 +0300

    w

---
+++ src/app/movie.datasource.ts
@@ -0,0 +1,10 @@
+import { Movie } from './movie'; 
+
+export const Movies: Movie [] = [
+    {id: 1, name: "Oppenheimer"},
+    {id: 2, name: "Barbie"},
+    {id: 3, name: "Saw"},
+    {id: 4, name: "Mission Impossible"},
+    {id: 5, name: "Transformers"}
+
+];
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,10 +1,35 @@
 import { Component } from "@angular/core";
+import {Movies} from '../movie.datasource';
+import { Movie } from "../movie";
 
-@Component ({
-    selector: 'movies',
-    templateUrl: 'movies.component.html',
-    styles: ['h2{color:blue; }']
+@Component({
+  selector: 'movies',
+  templateUrl: 'movies.component.html',
+  styles: [`
+    .list-group-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .list-group-item .badge-primary {
+      margin-right: 10px;
+      color: black;
+    }
+
+    .close {
+      cursor: pointer;
+      color: red;
+      font-weight: bold;
+    }
+  `]
 })
 export class MoviesComponent {
+  title = 'Movie List';
+  movies = Movies;
+  selectedMovie: Movie | null = null; 
 
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+  }
 }
